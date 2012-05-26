dc014bb6ce5838ff0e6377bf700bfa0c850d8987
Damon Oehlman <damon.oehlman@sidelab.com>
2012-05-26 19:56:11 +1000

    Tweaking to use talk resources

--- resources/js/pres.js
+++ resources/js/pres.js
@@ -3,40 +3,13 @@
       return function() {};
   }
   
-  $(function() {
-      $('section[data-bg]').each(function() {
-          var el = $(this),
-              bgImage = el.data('bg') || el.attr('data-bg');
-          
-          el.css('background-image', 'url(' + bgImage + ')');
-      });
-  
-      var activeDemo = '';
-      Reveal.addEventListener('slidechanged', function(evt) {
-          // find the active slide
-          var demo = $('section.present').data('demo');
-          
-          if (demo && activeDemo !== demo && window.opener) {
-              window.opener.postMessage('demo.' + demo, '*');
-          }
-      });
-  });
-  
-  /*
-  shout('html5-messaging', {
-      server: 'ws://localhost:8000/',
-      pcode:  1234,
-      keymap: 'reveal.js'
-  });
-  */
-  
   var url = 'http://localhost:5984/presentations/_changes?feed=eventsource&include_docs=true',
       remoteEvents = new EventSource(url);
   
   var updateEventId;
   remoteEvents.onmessage = function(evt) {
       if (updateEventId && evt.lastEventId !== updateEventId) {
-          window.location.reload();
+          // window.location.reload();
       }
       else {
           updateEventId = evt.lastEventId;
--- src/pres.js
+++ src/pres.js
@@ -2,31 +2,12 @@ function debug() {
     return function() {};
 }
 
-$(function() {
-    $('section[data-bg]').each(function() {
-        var el = $(this),
-            bgImage = el.data('bg') || el.attr('data-bg');
-        
-        el.css('background-image', 'url(' + bgImage + ')');
-    });
-
-    //= ../code/xdocmessaging/slidechange.js
-});
-
-/*
-shout('html5-messaging', {
-    server: 'ws://localhost:8000/',
-    pcode:  1234,
-    keymap: 'reveal.js'
-});
-*/
-
 //= ../code/eventsource/create-couch
 
 var updateEventId;
 remoteEvents.onmessage = function(evt) {
     if (updateEventId && evt.lastEventId !== updateEventId) {
-        window.location.reload();
+        // window.location.reload();
     }
     else {
         updateEventId = evt.lastEventId;
